ca34e2ed54a55887ec59bdb7dd568793110c502c
leon <leontimnaluithlen@gmail.com>
2021-02-22 14:44:23 +0100

    removed Logger step

--- requestCheck.sh
+++ requestCheck.sh
@@ -1,31 +1,50 @@
+#!/bin/bash
 
-host=https://localhost:8080
-#host=https://quiet-headland-19155.herokuapp.com
+#host=https://localhost:8080
+host=https://quiet-headland-19155.herokuapp.com
 
 context0=$(printf '"1.0", "0.0", "0.0", "1.0", "1.0", %.0s' {1..100})
 context0='"1.0", "0.0"  '
 curl --header "Content-Type: application/json" --request POST --data "{\"context\":[${context0::-2}]}" "$host/action"
 
+# Define a timestamp function
+timestamp() {
+  date + # current time
+}
 
-for i in {0..1}
+
+for j in {0..600}
     do
-        context1=$(printf '"1.0", "1.0", "1.0", "0.0", "0.0", %.0s' {1..100})
-        context2=$(printf '"0.0", "0.0", "0.0", "1.0", "1.0", %.0s' {1..100})
-        curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"0\", \"context\":[${context1::-2}], \"reward\":\"1.0\"}" "$host/update" &
-        curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"0\", \"context\":[${context2::-2}], \"reward\":\"0.0\"}" "$host/update" &
-        curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"1\", \"context\":[${context1::-2}], \"reward\":\"0.0\"}" "$host/update" &
-        curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"1\", \"context\":[${context2::-2}], \"reward\":\"1.0\"}" "$host/update" &
+
+    for i in {0..10}
+        do
+            context1=$(printf '"1.0", "1.0", "1.0", "0.0", "0.0", %.0s' {1..100})
+            context2=$(printf '"0.0", "0.0", "0.0", "1.0", "1.0", %.0s' {1..100})
+            context1='"1.0", "0.0"  '
+            context2='"0.0", "1.0"  '
+            curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"0\", \"context\":[${context1::-2}], \"reward\":\"1.0\"}" "$host/update" &
+            curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"0\", \"context\":[${context2::-2}], \"reward\":\"0.0\"}" "$host/update" &
+            curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"1\", \"context\":[${context1::-2}], \"reward\":\"0.0\"}" "$host/update" &
+            curl --header "Content-Type: application/json" --request POST --data "{\"modelId\":\"1\", \"context\":[${context2::-2}], \"reward\":\"1.0\"}" "$host/update" &
+        done
+    wait
+
+    start=$SECONDS
+
+    for i in {0..100}
+        do
+            curl --header "Content-Type: application/json" --request POST --data "{\"context\":[${context0::-2}]}" "$host/action" &
+        done
+    wait
+
+    duration=$(( SECONDS - start ))
+
+    echo $duration
+
+    printf "j: "
+    printf $j
+    printf "   "
+    dt=$(date '+%d/%m/%Y %H:%M:%S');
+    echo "$dt"
+    sleep 30
     done
-wait
-
-start=$SECONDS
-
-for i in {0..100}
-    do
-        curl --header "Content-Type: application/json" --request POST --data "{\"context\":[${context0::-2}]}" "$host/action" &
-    done
-wait
-
-duration=$(( SECONDS - start ))
-
-echo $duration
